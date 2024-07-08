c0486f07017540dd5a5295bc7068bce9cfb380dd
eightMealsADay <152494140+eightMealsADay@users.noreply.github.com>
2024-07-08 16:55:36 +0800

    Create result表item表 文彧婕.sql

---
+++ result表item表 文彧婕.sql
@@ -0,0 +1,19 @@
+Create table result_(
+Result_id integer,
+Gun_result interval,
+Net_result interval,
+Player_id integer,
+Event_id integer,
+Rank integer,
+Gender_rank integer,
+Primary key(Result_id),
+FOREIGN KEY(player_id) REFERENCES Player(ID),
+FOREIGN KEY(event_id) REFERENCES Event(ID),
+)；
+
+Create table Item_(
+ID integer,
+Name varchar(10),
+Primary key(ID),
+)；
+
