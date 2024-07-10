24eceed783127927b9db816d5113c766191c9d2b
ctltcl <3281875868@qq.com>
2024-07-10 20:16:32 +0800

    写了一些controllers
    2024 7 10

---
+++ Controllers/DriveController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using SqlSugar;
+using db_marathon.Models;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Security.Cryptography;
+
+
+namespace db_marathon.Controllers
+{
+    public class DriveController : Controller
+    {
+        //添加drive
+        public bool add_drive(int Volunteer_Id, int Shuttlecar_Id)
+        {
+            //获取数据库连接
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();
+
+            Drive drive = new Drive();
+            drive.Volunteer_Id = Volunteer_Id;
+            drive.Shuttlecar_Id = Shuttlecar_Id;
+
+            int count = db.Insertable(drive).ExecuteCommand();
+            if (count == 1)
+                return true;
+            else
+                return false;
+        }
+
+        //查询volunteer_id的Shuttlecar_Id
+        [HttpGet]
+        public int get_by_volunteer_id(int Volunteer_Id)
+        {
+            Drive drive = new Drive();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            drive = db.Queryable<Drive>().Where(it => it.Volunteer_Id == Volunteer_Id).First();
+            if (drive != null)
+            {
+                return drive.Shuttlecar_Id;
+            }
+            else
+            {
+                return -1; // 或者返回一个适当的错误信息
+            }
+        }
+
+        //查询Shuttlecar_Id的volunteer_id
+        [HttpGet]
+        public int get_by_shuttlecar_Id(int Shuttlecar_Id)
+        {
+            Drive drive = new Drive();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            drive = db.Queryable<Drive>().Where(it => it.Shuttlecar_Id == Shuttlecar_Id).First();
+            if (drive != null)
+            {
+                return drive.Volunteer_Id;
+            }
+            else
+            {
+                return -1; // 或者返回一个适当的错误信息
+            }
+        }
+
+        //删除drive
+        [HttpDelete]
+        public bool delete_drive(int Volunteer_Id, int Shuttlecar_Id)
+        {
+            //获取数据库连接
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();
+            //删除
+            try
+            {
+                int count = db.Deleteable<Drive>().Where(it => it.Volunteer_Id == Volunteer_Id && it.Shuttlecar_Id == Shuttlecar_Id).ExecuteCommand();
+                if (count == 1)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Controllers/VolunteerController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using SqlSugar;
+using db_marathon.Models;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Security.Cryptography;
+
+
+namespace db_marathon.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        //添加volunteer(注册时候输入Name，Telephone_Number，Password，平台分配id、Supplypoint_Id、Medicalpoint_Id)
+        [HttpPost]
+        public bool add_volunteer(int Id,string Name, string Telephone_Number,string Password)
+        {
+            //获取数据库连接
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();
+
+            Volunteer volunteer = new Volunteer();
+            volunteer.Id = Id;
+            volunteer.Name = Name;
+            volunteer.Telephone_Number = Telephone_Number;
+            volunteer.Password = Password;
+            volunteer.Medicalpoint_Id = null;
+            volunteer.Supplypoint_Id = null;//注册的时候置为空
+
+            int count = db.Insertable(volunteer).ExecuteCommand();
+            if (count == 1)
+                return true;
+            else
+                return false;
+        }
+
+        //登录
+        [HttpGet]
+        public bool login(string Name, string Telephone_Number, string Password)
+        {
+            Volunteer volunteer = new Volunteer();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            volunteer = db.Queryable<Volunteer>().Where(it => it.Name == Name && it.Telephone_Number == Telephone_Number && it.Password == Password).First();
+            if (volunteer != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false; // 或者返回一个适当的错误信息
+            }
+        }
+
+        //查询name的志愿者
+        [HttpGet]
+        public int get_by_name(string Name)
+        {
+            Volunteer volunteer = new Volunteer();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            volunteer = db.Queryable<Volunteer>().Where(it => it.Name == Name).First();
+            if (volunteer != null)
+            {
+                return volunteer.Id;
+            }
+            else
+            {
+                return -1; // 或者返回一个适当的错误信息
+            }
+        }
+
+        //查询id的志愿者
+        [HttpGet]
+        public string get_by_id(int Id)
+        {
+            Volunteer volunteer = new Volunteer();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            volunteer = db.Queryable<Volunteer>().Where(it => it.Id == Id).First();
+            if (volunteer != null)
+            {
+                return volunteer.Name;
+            }
+            else
+            {
+                return "无"; // 或者返回一个适当的错误信息
+            }
+        }
+
+        //分配Supplypoint和Medicalpoint
+        [HttpPatch]
+        public bool volunteer_supplypoint(int Id, int Supplypoint_Id)
+        {
+            Volunteer volunteer = new Volunteer();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            volunteer = db.Queryable<Volunteer>().Where(it => it.Id == Id).First();
+            if (volunteer != null && volunteer.Medicalpoint_Id == null)
+            {
+                volunteer.Supplypoint_Id= Supplypoint_Id;
+                return true;
+            }
+            else
+            {
+                return false; // 或者返回一个适当的错误信息
+            }
+        }
+
+        [HttpPatch]
+        public bool volunteer_medicalpoint(int Id, int Medicalpoint_Id)
+        {
+            Volunteer volunteer = new Volunteer();
+            dbORM dborm = new dbORM();
+            SqlSugarClient db = dborm.getInstance();//获取数据库连接
+
+            volunteer = db.Queryable<Volunteer>().Where(it => it.Id == Id).First();
+            if (volunteer != null && volunteer.Supplypoint_Id == null)
+            {
+                volunteer.Medicalpoint_Id = Medicalpoint_Id;
+                return true;
+            }
+            else
+            {
+                return false; // 或者返回一个适当的错误信息
+            }
+        }
+
+        
+        
+    }
+
+}
