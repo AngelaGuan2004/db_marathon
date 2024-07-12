a06b0cd0742d38aa2250b4949137ff7b10aa687b
AngelaGuan2004 <149296585+AngelaGuan2004@users.noreply.github.com>
2024-07-13 00:05:37 +0800

    重新建asp.net项目时自动更改的一些不知道是什么的东西

--- root/backend/backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ root/backend/backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1f1b384bab04a653bb31362a905d8690318ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172178250fbbb999c5dc2284d99a20abcad287da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
