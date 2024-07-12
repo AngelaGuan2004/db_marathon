92a8e7fc61b6acd73c9b12c7ce5b9e18cf1c93f2
AngelaGuan2004 <149296585+AngelaGuan2004@users.noreply.github.com>
2024-07-12 09:19:14 +0800

    711controllers

---
+++ root/backend/backend/Controllers/MedicalController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SqlSugar;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging; 
+using backend;
+using Microsoft.AspNetCore.Cors;
+using db_marathon.Models;
+using System.Numerics;
+
+namespace MarathonMaster.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("any")]
+    public class MedicalController : ControllerBase
+    {
+        private readonly SqlSugarClient _db;
+        private readonly ILogger<PlayerController> _logger; // 声明日志记录器
+
+        public MedicalController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+            dbORM dborm = new dbORM();
+            _db = dborm.getInstance();
+        }
+        [HttpPost]
+        public async Task<IActionResult> add_injury([FromBody] Medical_Service service)
+        {// 伤员id 医疗点id
+            
+            _logger.LogInformation("收到伤员数据");
+
+            try
+            {
+                await _db.Insertable(service).ExecuteCommandAsync();
+                _logger.LogInformation("成功插入伤员数据"); 
+                return Ok(true);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex, "插入数据失败: {@Medical_Service}", service); // 记录错误信息
+
+                return BadRequest(false);
+            }
+        }
+    }
+    
+}
---
+++ root/backend/backend/Controllers/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SqlSugar;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging; 
+using backend;
+using Microsoft.AspNetCore.Cors;
+using db_marathon.Models;
+using System.Numerics;
+
+namespace MarathonMaster.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("any")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly SqlSugarClient _db;
+        private readonly ILogger<PlayerController> _logger; // 声明日志记录器
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+            dbORM dborm = new dbORM();
+            _db = dborm.getInstance();
+        }
+        [HttpPost]//添加Player（注册需要手动输入Name、Id_Number、Password、Gender平台分配id）
+        public async Task<IActionResult> add_player([FromBody] Player player)
+        {
+            
+            _logger.LogInformation("收到数据: {@Player}", player); // 记录收到的数据
+
+            try
+            {
+                await _db.Insertable(player).ExecuteCommandAsync();
+                _logger.LogInformation("成功插入数据: {@Player}", player); // 记录插入成功
+                return Ok(true);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex, "插入数据失败: {@Player}", player); // 记录错误信息
+
+                return BadRequest($"插入失败: {ex.Message}");
+            }
+        }
+    }
+    
+}
