a3bbd32d24a813e532eebc4920fb86e67d67075d
eightMealsADay <152494140+eightMealsADay@users.noreply.github.com>
2024-08-17 17:52:24 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,17 @@ Vue.use(Router);
 import PhotoWall from '@/view/PhotoWall.vue'
 import PhotographerCenter from '@/view/PhotographerCenter.vue'
 import MyPhotographyWorks from '@/view/MyPhotographyWorks.vue'
+import InjuryEntry from '@/view/InjuryEntry.vue'
 
 export default new Router({
     routes:[
         { path:'/', component:PhotoWall},
         { path:'/photoWall', component:PhotoWall},
         { path:'/photographerCenter', component:PhotographerCenter},
-        { path:'/photographerCenter/myPhotographyWorks', component:MyPhotographyWorks}
+        { path:'/photographerCenter/myPhotographyWorks', component:MyPhotographyWorks},
+        { path:'/injuryEntry',component:InjuryEntry},
     ]
 })
+
+
 
