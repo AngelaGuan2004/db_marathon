eca2ba45bb57cd52cf89a0857d6972bce96b563a
gracegaogreen <152162558+gracegaogreen@users.noreply.github.com>
2024-08-18 15:46:18 +0800

    Update index.js

--- root/frontend/src/router/index.js
+++ root/frontend/src/router/index.js
@@ -1,11 +1,51 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import EventList from '@/view/EventManagement/EventList.vue';
+import ParticipantLottery from '@/view/EventManagement/ParticipantLottery.vue';
+import PacerSelection from '@/view/EventManagement/PacerSelection.vue';
+import PacerSubmission from '@/view/EventManagement/PacerSubmission.vue';
+import EmergencyRunnerSelection from '@/view/EventManagement/EmergencyRunnerSelection.vue';
+import EmergencyRunnerSubmission from '@/view/EventManagement/EmergencyRunnerSubmission.vue';
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-        }
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'EventList',
+      component: EventList
+    },
+    {
+      path: '/participantLottery',
+      name: 'ParticipantLottery',
+      component: ParticipantLottery,
+      props: true
+    },
+    {
+      path: '/participantLottery/pacerSelection',
+      name: 'PacerSelection',
+      component: PacerSelection,
+      props: true
+    },
+    {
+      path: '/participantLottery/pacerSubmission',
+      name: 'PacerSubmission',
+      component: PacerSubmission,
+      props: true
+    },
+    {
+      path: '/participantLottery/emergencyRunnerSelection',
+      name: 'EmergencyRunnerSelection',
+      component: EmergencyRunnerSelection,
+      props: true
+    },
+    {
+      path: '/participantLottery/emergencyRunnerSubmission',
+      name: 'EmergencyRunnerSubmission',
+      component: EmergencyRunnerSubmission,
+      props: true
+    }
+  ]
 });
