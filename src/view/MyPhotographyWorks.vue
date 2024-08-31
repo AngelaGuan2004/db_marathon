12c0b6faa81472ed4895fc7f7d0c3504a84a0841
eightMealsADay <152494140+eightMealsADay@users.noreply.github.com>
2024-08-31 21:58:05 +0800

    Update MyPhotographyWorks.vue

--- src/view/MyPhotographyWorks.vue
+++ src/view/MyPhotographyWorks.vue
@@ -43,12 +43,13 @@
               column-gap: 16px; 
               row-gap: 15px;
               padding: 10px;">
-            <div class="photo-frame" v-for="(photo, index) in myphotos" :key="index">
-              <img :src="photo.src" alt="Photo" class="photo" @click="openPreview(photo)"/>
+            <div class="photo-frame" v-for="photo in myphotos" :key="photo.id">
+              <img :src="photo.address" alt="Photo" class="photo" @click="openPreview(photo)"/>
               <div class="info-box">
                 <div style="text-align: left; font-size: 14px; padding-left: 5px; padding-top: 5px;">
-                  <p>日期: {{ photo.date }}</p>
+                  <p>日期: {{ photo.time }}</p>
                   <p>地点: {{ photo.location }}</p>
+                  <p>赛事: {{ photo.event_id}}</p>
                   <p style="color:crimson">❤️: {{ photo.likes}}</p>
                 </div>
                
@@ -57,7 +58,7 @@
           </div>
           <!-- 图片预览框 -->
           <el-dialog :visible.sync="dialogVisible" width="60%" center>
-            <img :src="currentPhoto.src" alt="Preview" style="width: 100%;" />
+            <img :src="currentPhoto.address" alt="Preview" style="width: 100%;" />
           </el-dialog>
           <!-- 翻页 -->
           <div style="display: flex; justify-content: center; margin-top: 10px;">
@@ -77,20 +78,24 @@
 
 
 <script>
+import { inquiryPhotoByPhotographer } from '@/api/Photo';
+
 
 export default {
     name:'MyPhotographyWorks',
     data(){
       return{
-        myphotos: [
-          { src: require('@/assets/1.jpg'), date: '2023-07-10', location: '地点1', likes: 1000 },
-          { src: require('@/assets/3.jpg'), date: '2023-07-12', location: '地点3', likes: 800 },
-          { src: require('@/assets/5.jpg'), date: '2023-07-14', location: '地点5', likes: 600 },
-          { src: require('@/assets/7.jpg'), date: '2023-07-15', location: '地点7', likes: 499 },
-          { src: require('@/assets/9.jpg'), date: '2023-07-17', location: '地点9', likes: 50 },
-          { src: require('@/assets/10.jpg'), date: '2023-06-10', location: '地点3', likes: 666},
-          { src: require('@/assets/11.jpg'), date: '2023-05-22', location: '地点1', likes: 888 },
-        ],
+        photographerID: 73,
+        // myphotos: [
+        //   { src: require('@/assets/1.jpg'), date: '2023-07-10', location: '地点1', likes: 1000 },
+        //   { src: require('@/assets/3.jpg'), date: '2023-07-12', location: '地点3', likes: 800 },
+        //   { src: require('@/assets/5.jpg'), date: '2023-07-14', location: '地点5', likes: 600 },
+        //   { src: require('@/assets/7.jpg'), date: '2023-07-15', location: '地点7', likes: 499 },
+        //   { src: require('@/assets/9.jpg'), date: '2023-07-17', location: '地点9', likes: 50 },
+        //   { src: require('@/assets/10.jpg'), date: '2023-06-10', location: '地点3', likes: 666},
+        //   { src: require('@/assets/11.jpg'), date: '2023-05-22', location: '地点1', likes: 888 },
+        // ],
+        myphotos:[],
         select: '2', // 默认排序为最热
         dialogVisible: false,
         currentPhoto: {}
@@ -101,9 +106,12 @@ export default {
         // 将 myphotos 按日期降序排列，并取出最近的 6 张照片
         return this.myphotos
           .slice() // 创建 myphotos 的浅拷贝
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
+          .sort((a, b) => new Date(b.time) - new Date(a.time))
           .slice(0, 6);
       }
+    },
+    async mounted() {
+      await this.fetchPhotosByPhotographer(this.photographerID);
     },
      methods:{
       navigateTo(_path) {
@@ -112,7 +120,7 @@ export default {
       sortPhotos() {
         if (this.select === '1') {
           // 按日期排序，最新的在前
-          this.myphotos.sort((a, b) => new Date(b.date) - new Date(a.date));
+          this.myphotos.sort((a, b) => new Date(b.time) - new Date(a.time));
         } else if (this.select === '2') {
           // 按点赞数排序，最多的在前
           this.myphotos.sort((a, b) => b.likes - a.likes);
@@ -121,9 +129,29 @@ export default {
       openPreview(photo) {
         this.currentPhoto = photo;
         this.dialogVisible = true;
+      },
+      async fetchPhotosByPhotographer(ID) {
+        try {
+          const response = await inquiryPhotoByPhotographer(ID);
+          this.myphotos = response;
+          console.log("收到的照片数据:", this.myphotos);
+        } catch (error) {
+          console.error('获取摄影作品时发生错误:', error);
+          this.$message.error('获取摄影作品失败');
+        }
       }
     }
 }
+    //一张photo的数据：
+    // {
+    //     "id": 8,
+    //     "event_id": "10091",
+    //     "time": "2023/3/26 0:00:00",
+    //     "location": "联创街",
+    //     "photographer_id": 73,
+    //     "address": "C:\\Users\\Administrator\\Desktop\\DBexcel\\photo\\91\\8.jpg",
+    //     "likes": 921
+    // }
 </script>
 
 
@@ -163,6 +191,7 @@ export default {
     display: flex;
     margin-top: 80px;
   }
+
   .sidebar {
     width: 200px;
     height:100vh;
@@ -284,7 +313,7 @@ export default {
   .info-box{
     margin-top: 6px; 
     width: 100%;
-    height: 18%;
+    height: 22%;
     background-color: #f2f2f2;
     border-radius: 8px; 
   }
