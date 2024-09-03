84f81a95b0e5fa67c58dd8618e3101b423f3cd97
eightMealsADay <152494140+eightMealsADay@users.noreply.github.com>
2024-09-03 11:32:56 +0800

    Update PhotographerCenter.vue

--- src/view/PhotographerCenter.vue
+++ src/view/PhotographerCenter.vue
@@ -1,67 +1,56 @@
 <template>
-  <div>
-    <nav class="navbar">
-      <img src="@/assets/logo.png" alt="Logo" class="logo" style="height: 55px;">
-      <button @click="navigateTo('/')">首页</button>
-      <button @click="navigateTo('/profile')">个人空间</button>
-      <button @click="navigateTo('/photoWall')">赛事摄影</button>
-      <button @click="navigateTo('/injuryEntry')">伤员</button>
-    </nav>
-    
-    <div class="main-content">
-      <div class="sidebar">
-        <button @click="navigateTo('/photoWall')" class="sidebar-button">照片墙</button>
-        <button @click="navigateTo('/photographerCenter')" class="sidebar-button" style="font-weight:bold;">摄影师中心</button>
+  <div id="PhotographerCenter">
+    <div class="MainContent">
+      <div class="PhotographerInfo">
+        <div class="PhotographerInfoUpper">
+          <div class="PhotographerInfoTitle">基本信息</div>
+        </div>
+        <hr class="line" />
+        <div class="PhotographerInfoText">
+          <div class="a-button">
+            <p class="down-notice">姓名</p>
+            <p class="down-notice2">{{ name }}</p>
+          </div>
+          <div class="a-button">
+            <p class="down-notice">ID</p>
+            <p class="down-notice2">{{ photographer_Id }}</p>
+          </div>
+        </div>
       </div>
-
-      <div class="content">
-        <div class="myPhotographyWorks" @click="navigateTo('/photographerCenter/myPhotographyWorks')">
-          <h2 style="text-align:left; font-size: 24px; color: #565656;">我的摄影作品</h2>
+      <div class="PhotographerContent">
+        <div class="myPhotographyWorks" @click="navigateTo('/UserTab/myPhotographyWorks')">
+          <h2 style="font-size: 24px;margin-left: 10px;">我的摄影作品</h2>
           <div class="photo-bar">
             <div class="photo-frame" v-for="(photo, index) in photos" :key="index">
-              <img :src="photo.src" alt="Photo" class="photo" />
+              <img :src="photo.address" alt="Photo" class="photo" />
             </div>
           </div>
-          <button @click="navigateTo('/photographerCenter/myPhotographyWorks')" style="font-size: 16px; color: #007bff; background: none; border: none; margin-top:10px;">&gt;点击查看详情...</button>
+          <button @click="navigateTo('/UserTab/myPhotographyWorks')"
+            style="font-size: 16px; color: #007bff; background: none; border: none; margin-top:10px;">&gt;点击查看详情...</button>
         </div>
-    
         <div class="uploadNewPhotography">
-          <h2 style="text-align:left; font-size: 24px; color: #565656;">上传新摄影</h2>
+          <h2 style="font-size: 24px;margin-left: 10px;">上传新摄影</h2>
           <div class="upload-button-container">
             <div class="upload-button" @click="handleUpload()">+</div>
-
             <el-dialog :visible.sync="formVisible" title="（单次上传一张摄影作品）">
-              <el-form  ref="form" :model="form" :rules="rules" label-width="80px">
-
+              <el-form ref="form" :model="form" :rules="rules" label-width="100px">
                 <el-form-item label="摄影时间" prop="time">
                   <el-col :span="15">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.time" style="width: 100%;"></el-date-picker>
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.time"
+                      style="width: 100%;"></el-date-picker>
                   </el-col>
                 </el-form-item>
-
                 <el-form-item label="赛事" prop="event">
                   <el-select v-model="form.event" placeholder="请选择赛事">
-                    <el-option
-                      v-for="event in events"
-                      :key="event.value"
-                      :label="event.label"
-                      :value="event.value">
-                    </el-option>             
+                    <el-option v-for="event in events" :key="event.value" :label="event.name" :value="event.id">                    </el-option>
                   </el-select>
                 </el-form-item>
-
                 <el-form-item label="拍摄地点" prop="location">
                   <el-input v-model="form.location"></el-input>
                 </el-form-item>
-
                 <el-form-item>
-                  <el-upload action="#"
-                       list-type="picture-card"
-                       limit="1"
-                       :on-success="handleSuccess"
-                       :file-list="fileList"
-                       :on-change="handleChange"
-                       :auto-upload="false">
+                  <el-upload action="#" list-type="picture-card" limit="1" :on-success="handleSuccess"
+                    :file-list="fileList" :on-change="handleChange" :auto-upload="false">
                     <i slot="default" class="el-icon-plus"></i>
                     <div slot="file" slot-scope="{file}">
                       <img class="el-upload-list__item-thumbnail" :src="file.url" alt="">
@@ -75,46 +64,43 @@
                       </span>
                     </div>
                   </el-upload>
-
                   <el-dialog :visible.sync="dialogVisible">
                     <img width="100%" :src="dialogImageUrl" alt="">
                   </el-dialog>
-
                 </el-form-item>
-
                 <el-form-item>
-                  <el-button type="primary" @click="onSubmit">上传</el-button>
-                  <el-button @click="handleCancel">取消</el-button>
+                  <el-button type="primary" @click="onSubmit" class="PhotographerCenterButton">上传</el-button>
+                  <el-button @click="handleCancel" class="PhotographerCenterButton">取消</el-button>
                 </el-form-item>
               </el-form>
             </el-dialog>
-
           </div>
-
         </div>
-
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
+//import { getPhotographerInfor } from '@/api/UserCenter.js'
+import { getAllEvents, inquiryPhotoByPhotographer } from '@/api/Photo';
+
 export default {
   name: 'PhotographerCenter',
-  props: {msg: String},
+  props: { msg: String },
   data() {
     return {
-      subWeb: 'PhotoWall',
-      photos: [
-        { src: require('@/assets/1.jpg') },
-        { src: require('@/assets/2.jpg') },
-        { src: require('@/assets/3.jpg') },
-        { src: require('@/assets/4.jpg') },
-        { src: require('@/assets/5.jpg') },
-        { src: require('@/assets/6.jpg') },
-      ],
+      name: '',
+      photographer_Id: 73,//暂时把摄影师用户信息写死
+      photos:[],
+      // photos: [
+      //   { src: require('@/assets/images/1.jpg') },
+      //   { src: require('@/assets/images/2.jpg') },
+      //   { src: require('@/assets/images/3.jpg') },
+      //   { src: require('@/assets/images/4.jpg') },
+      //   { src: require('@/assets/images/5.jpg') },
+      //   { src: require('@/assets/images/6.jpg') },
+      // ],
 
       dialogImageUrl: '',
       dialogVisible: false,
@@ -122,47 +108,69 @@ export default {
       disabled: false,
       fileSelected: false,
       fileList: [],
-      event:[],
+      events: [],
 
       form: {
         time: '',
         event: '',
         location: '',
-        desc: ''
       },
       rules: {
         time: [
           { required: true, message: '请选择摄影时间', trigger: 'change' }
         ],
         event: [
-          { required: true, message: '请选择照片所属赛事',trigger: 'change'}
+          { required: true, message: '请选择照片所属赛事', trigger: 'change' }
         ],
         location: [
-          {required:true,massage:'请输入拍摄地点',trigger: 'blur'}
+          { required: true, message: '请输入拍摄地点', trigger: 'blur' }
         ]
       }
     }
   },
+  async mounted(){
+    try { 
+      // //获取当前摄影师照片
+      // const photoResponse = await inquiryPhotoByPhotographer(this.photographer_Id);
+      // this.photos=photoResponse.map(photo=>{
+      //   return {
+      //       ...photo,
+      //       time: photo.time.split(' ')[0],  // 只保留年月日部分
+      //       address:'http://'+photo.address
+      //   };
+      // });
+      // console.log('收到的照片',this.photos);
 
+    // 获取所有赛事
+      const eventsResponse = await getAllEvents();
+      this.events = eventsResponse.map(event => ({
+        id: event.id,
+        name: event.name
+      }));
+      console.log('赛事信息：',this.events);
+    } catch (error) {
+      console.error('获取赛事失败:', error);
+    }
+  },
   methods: {
     navigateTo(_path) {
-      this.$router.push({path:_path},()=>{})
+      this.$router.push({ path: _path }, () => { })
     },
-    handleUpload(){
-      this.formVisible=true;
+    handleUpload() {
+      this.formVisible = true;
     },
     handleRemove(file) {
-    this.fileList = this.fileList.filter(f => f.uid !== file.uid);
-    this.fileSelected = this.fileList.length > 0;
+      this.fileList = this.fileList.filter(f => f.uid !== file.uid);
+      this.fileSelected = this.fileList.length > 0;
     },
     handlePictureCardPreview(file) {
       this.dialogImageUrl = file.url;
       this.dialogVisible = true;
     },
     handleSuccess(res, file) {
-    this.fileList.push(file);
-    this.fileSelected = true;
-    this.imageUrl = URL.createObjectURL(file.raw);
+      this.fileList.push(file);
+      this.fileSelected = true;
+      this.imageUrl = URL.createObjectURL(file.raw);
     },
     beforeUpload(file) {
       const isJPG = file.type === 'image/jpeg';
@@ -175,13 +183,13 @@ export default {
       if (!isLt10M) {
         this.$message.error('上传摄影作品大小不能超过 10MB!');
       }
-      
-      this.fileSelected=true;
+
+      this.fileSelected = true;
       return (isJPG || isPNG) && isLt10M;
     },
     handleChange(file, fileList) {
       //const isValid = this.beforeUpload(file);
-      const isValid=true;
+      const isValid = true;
       if (isValid) {
         this.fileList = fileList;
         this.fileSelected = true;
@@ -205,157 +213,32 @@ export default {
       });
     },
     resetForm() {
-    this.$refs.form.resetFields();
-    this.fileList = [];
-    this.fileSelected = false;
+      this.$refs.form.resetFields();
+      this.fileList = [];
+      this.fileSelected = false;
     },
     handleCancel() {
       this.formVisible = false;
       this.resetForm();
     }
-  }
+  },
+  // mounted() {
+  //   this.ID = localStorage.getItem('UserId')
+  //   getPhotographerInfor(this.ID)
+  //     .then((res) => {
+  //       this.name = res.Name;
+  //     })
+  //     .catch(error => {
+  //       console.error('查询失败:', error);
+  //       alert('查询失败');
+  //     });
+  // }
 }
 </script>
 
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html, body {
-  height: 100%;
-}
-
-.navbar {
-  background-color: #c81623; 
-  padding: 20px;
-  display: flex;
-  justify-content: space-around;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1000; /* 确保 navbar 在最上层 */
-}
-
-.main-content {
-  display: flex;
-  margin-top: 80px;
-}
-
-.sidebar {
-  width: 200px;
-  height:100vh;
-  position:fixed;
-  background-color: #ffebeb;
-  padding: 20px;
-  box-shadow: 2px 0 5px rgba(0,0,0,0.1);
-}
-
-.sidebar-button {
-  width: 100%;  background-color: #ffebeb; 
-  color: #565656; 
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  font-size: 18px;
-  margin-bottom: 10px;
-  margin-top: 20px;
-  border-radius: 5px; 
-}
-
-.sidebar-button:hover {
-  background-color: #ffd7d7;
-  font-weight: bold;
-}
-
-.content {
-  flex: 1;
-  padding: 20px;
-}
-
-.photo-bar{
-  display: flex;
-  gap: 10px; 
-  margin-top: 10px;
-  overflow-x: auto;
-  white-space: nowrap;
-}
-
-.photo-frame {
-  display:inline-block;
-  width: 130px;
-  height: 180px;
-  position: relative;
-  overflow: hidden;
-  border: 2px;
-  border-radius: 5px; 
-  background-color: rgb(210, 210, 210);
-  margin-left: 17px;
-}
-
-.photo {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-button {
-  background-color: #c81623;
-  color:white;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  font-size: 16px;
-}
-
-.upload-button-container {
-  display: flex;
-  justify-content: flex-start;
-  margin-left: 20px;
-  margin-top: 15px;
-}
-
-.upload-button {
-  background-color: #c5c5c5;
-  color:white;
-  border: none;
-  border-radius: 8px;
-  padding: 30px 45px;
-  cursor: pointer;
-  font-size: 50px;
-  font-weight: bold;
-  margin-top: 20px;
-}
-
-button:hover {
- font-weight:bold;
-}
-
-.myPhotographyWorks{
-  background-color: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  width: 990px;
-  height:300px;
-  padding: 20px;
-  margin-top: 20px;
-  margin-left:210px;
-}
-
-.uploadNewPhotography {
-  background-color: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  width: 990px;
-  height:260px;
-  padding: 20px;
-  margin-top: 25px;
-  margin-left:210px;
-}
-
+@import 'element-ui/lib/theme-chalk/index.css';
+@import "@/assets/css/Base.css";
+@import "@/assets/css/PhotographerCenter.css";
 </style>
-
